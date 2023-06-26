55875aabfe887b024a3c95c131ab8f3c6be05887
DoukanFiliz <dogukanfiliz@hotmail.com>
2023-06-26 19:49:12 +0300

    init commit

---
+++ src/app/category/category.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Category } from './category';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent {
+  title = 'Kategori Listesi';
+  categories: Category[] = [
+    {id: 1, name: "Elektronik"},
+    {id: 2, name: "Bilgisayar"},
+    {id: 3, name: "İletişim"},
+    {id: 4, name: "Müzik"},
+    {id: 5, name: "İçecek"}
+  ]
+}
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Product } from './product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+  title = 'Ürün Listesi';
+  products: Product[] = [
+    {id: 1, name: "Laptop", price: 25000, categoryId: 2, description: "Lenovo Ideapad", imageUrl:"https://resim.epey.com/131751/m_lenovo-ideapad-330-81de0054tx-8.png"},
+    {id: 2, name: "Mouse", price: 2500, categoryId: 1, description: "Microsoft Modern Mobile Pastel Blue", imageUrl:"https://m.media-amazon.com/images/I/619T8JcKnmL._AC_UF1000,1000_QL80_.jpg"},
+    {id: 3, name: "Headphone", price: 330, categoryId: 1, description: "James Donkey", imageUrl:"https://productimages.hepsiburada.net/s/22/1500/9988887707698.jpg"},
+    {id: 4, name: "Mobile Phone", price: 40000, categoryId: 3, description: "Samsung S23 Lilac", imageUrl:"https://cdn.vatanbilgisayar.com/Upload/PRODUCT/samsung/thumb/137420-1_large.jpg"},
+    {id: 4, name: "Music", price: 16, categoryId: 4, description: "Spotify Account", imageUrl:"https://play-lh.googleusercontent.com/WB0j9PZGUofgwNbJ8Jm0-v03FIwBMj5Ovi5eso8xaBVTh6FGOyc4xyNjwMkTziKgkBs"},
+    {id: 5, name: "Drink", price: 26, categoryId: 5, description: "Red Bull", imageUrl:"https://images.migrosone.com/sanalmarket/product/08110030/08110030-a4b666-1650x1650.png"}
+  ];
+  deger: boolean = true;
+}
